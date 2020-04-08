7fcb9e4fab91335439db7588470b3777735b35f4
Vanessa Mendez <vanessamendez@vanessas-air.attlocal.net>
2020-04-08 02:14:31 -0500

    second load

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Web and Media Portfolio',
+    author: 'Cesar Castro',
+    description:
+      'this is my portfolio featuring web design, photography, videography',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
